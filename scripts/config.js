ed606b30d394e7b4e77ff79b58bf7a1881f7d158
Bradley Anderson <banderso@nerdery.com>
2014-09-13 23:22:49 -0500

    Adding requirejs's text plugin so we can require templates.

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,9 @@ requirejs.config({
 
     paths: {
         'jquery': 'lib/jquery-2.1.1.min',
-        'Handlebars': 'lib/Handlebars'
+        'Handlebars': 'lib/handlebars-v2.0.0',
+        'templates': '../templates',
+        'text': 'lib/text'
     }
 
 });
